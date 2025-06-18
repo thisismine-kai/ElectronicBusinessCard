4065a21873d76adb247db2c652d5b97c8038afab
kasiei <kaisei1147@gmail.com>
2025-06-18 23:03:22 +0900

    メッセージ

--- src/pages/MyBusinessCard.jsx
+++ src/pages/MyBusinessCard.jsx
@@ -11,7 +11,7 @@ function MyBusinessCard() {
   return (
     <div style={{
       display: 'flex',
-      minHeight: '200vh',
+      minHeight: '150vh',
       backgroundColor: "#f0f0f0",
       boxSizing: 'border-box',
       color: "#000000",
