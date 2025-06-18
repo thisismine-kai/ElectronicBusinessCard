13c4f15e0a67de136543a487675d95a3dbefe0fb
kasiei <kaisei1147@gmail.com>
2025-06-19 00:14:34 +0900

    メッセージ

--- src/pages/MyBusinessCard.jsx
+++ src/pages/MyBusinessCard.jsx
@@ -10,7 +10,6 @@ function MyBusinessCard() {
 
   return (
     <div style={{ 
-      display: 'flex', 
       minHeight: '100vh', 
       fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
       margin: 0,       // 追加
